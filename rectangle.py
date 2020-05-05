dfc8b0e4912da488bc3e36db2542f6e284cad3ee
sina <sina.ataran@yahoo.com>
2020-05-05 20:49:31 +0430

    Add setter and getter functions for rectangle width and height

--- rectangle.py
+++ rectangle.py
@@ -17,3 +17,17 @@ class Rectangle(object):
             raise TypeError('value should be float')
         if value <= 0:
             raise ValueError('value should be greater than zero')
+
+    def get_width(self) -> float:
+        return self._width
+
+    def set_width(self, _width: float):
+        Rectangle.check_value_is_pos_number(_width)
+        self._width = _width
+
+    def get_height(self) -> float:
+        return self._height
+
+    def set_height(self, _height: float):
+        Rectangle.check_value_is_pos_number(_height)
+        self._height = _height
