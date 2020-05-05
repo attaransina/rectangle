f4dc66ad8db22b589219706acc82b42f9198e104
sina <sina.ataran@yahoo.com>
2020-05-05 21:16:52 +0430

    Remove extra function in Rectangel class

--- shape/rectangle.py
+++ shape/rectangle.py
@@ -12,15 +12,6 @@ class Rectangle(Shape):
     def compute_area(self):
         return float(self._width * self._height)
 
-    @staticmethod
-    def check_value_is_pos_number(value):
-        if value is None:
-            raise TypeError('value should not be None')
-        if not isinstance(value, float):
-            raise TypeError('value should be float')
-        if value <= 0:
-            raise ValueError('value should be greater than zero')
-
     def get_width(self) -> float:
         return self._width
 
