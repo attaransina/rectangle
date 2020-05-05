83cfb9aa4abd6a689deeb7c28635a7ac26ea3abb
sina <sina.ataran@yahoo.com>
2020-05-05 20:32:48 +0430

    Add some unit-tests for input validation function in Rectangle class

--- test_rectangle.py
+++ test_rectangle.py
@@ -10,3 +10,19 @@ class TestRectangle(TestCase):
 
     def test_correct_initialize(self):
         rec = Rectangle(_width=12.0, _height=20.0)
+
+    def test_negative_width_initialize(self):
+        with self.assertRaises(ValueError):
+            rec = Rectangle(_width=-1.5, _height=2.5)
+
+    def test_negative_height_initialize(self):
+        with self.assertRaises(ValueError):
+            rec = Rectangle(_width=2.0, _height=-10.5)
+
+    def test_none_width_initialize(self):
+        with self.assertRaises(TypeError):
+            rec = Rectangle(_width=None, _height=2.2)
+
+    def test_none_height_initialize(self):
+        with self.assertRaises(TypeError):
+            rec = Rectangle(_width=2.0, _height=None)
