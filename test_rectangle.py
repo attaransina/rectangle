1022a4811a4f43899e0e3359ae1798e0d028c1b3
sina <sina.ataran@yahoo.com>
2020-05-05 20:34:53 +0430

    Add type checking test for input validation function in Rectangle class

--- test_rectangle.py
+++ test_rectangle.py
@@ -10,3 +10,11 @@ class TestRectangle(TestCase):
 
     def test_correct_initialize(self):
         rec = Rectangle(_width=12.0, _height=20.0)
+
+    def test_str_width_initialize(self):
+        with self.assertRaises(TypeError):
+            rec = Rectangle(_width='23.0', _height=24.2)
+
+    def test_int_with_initialize(self):
+        with self.assertRaises(TypeError):
+            rec = Rectangle(_width=12, _height=12.3)
